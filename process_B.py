845b9f482ca3b8fe9b046f0aec80d1bd416a20ca
R1dge919 <51169059+R1dge919@users.noreply.github.com>
2022-09-11 13:31:15 +0900

    process_B 更新
    サイズ、オフセットの取得を定数にしていたところを、ファイル解析で自動取得するように
    （こうすることで、ひょっとしたら、UE3の別ゲーでも使えるかもしれない）

--- process_B.py
+++ process_B.py
@@ -1,7 +1,7 @@
 import os
 from sys import byteorder
 
-# Fonts_en.gfxの処理
+# --------Fonts_en.gfxの処理--------
 # ① Fonts_en.gfxを開く（ファイルサイズ取得のため）
 with open ("./Fonts_en/Fonts_en.gfx", "rb") as f:
     s1=f.read()
@@ -14,6 +14,7 @@ with open ("./Fonts_en/Fonts_en.GFxMovieInfo",'rb') as f:
     s=f.read()
 head = s[:32] # s1で削除した先頭32bit
 tail = s[-8:] # s1で削除した末尾8bit
+size = len(s).to_bytes(4, byteorder="little") # ⑤で使う
 
 strings = head + s1 + tail # ② 削除した先頭・末尾を再配置
 strings = strings.replace(strings[28:32],old_size) # ③A アドレス1C～1Fに。old_sizeを書き込み
@@ -23,16 +24,17 @@ new_size = len(strings).to_bytes(4, byteorder="little") # ④ 新ファイルサ
 
 
 
-# Fonts_en.upkの処理
+# --------Fonts_en.upkの処理--------
 with open ("./unpacked/Fonts_en.upk", "rb") as f:
     s=f.read()
 
 upk_size = len(s).to_bytes(4, byteorder="little") # ③ Fonts_en.upkファイルサイズ
+offset = (s.find(b"\x47\x46\x58")-32).to_bytes(4, byteorder="little") # ⑥で使う。「"GFX"の位置-32」の16進数
 
 s = s + strings # ④ strings追記
 
-s = s.replace(b'\xCA\x04\x03\x00',new_size) # ⑤ size置換
-s = s.replace(b'\xD1\x37\x00\x00',upk_size) # ⑥ offset置換
+s = s.replace(size,new_size) # ⑤ size置換
+s = s.replace(offset,upk_size) # ⑥ offset置換
 
 if not os.path.exists('./out'): # 出力用ディレクトリが存在しない場合、作成する
     os.makedirs('./out')
